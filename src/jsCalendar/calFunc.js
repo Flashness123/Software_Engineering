50c1d2d7214c800afc92046e12b54b10cffc8acc
Duttixxl2 <s83832@htw-dresden.de>
2023-04-29 14:48:36 +0200

    search after date works

--- src/jsCalendar/calFunc.js
+++ src/jsCalendar/calFunc.js
@@ -1,7 +1,7 @@
+/////////////////////////////////Search////////////////////////////////////////
 //Listener for search button
 var searchButton = document.getElementById("searchEvent");
 searchButton.addEventListener("click", function () {
-	// document.getElementById("look").innerText = "Look for events";
 	var searchValue = document.getElementById("searchValue").value;
 	var pastDate = document.getElementById("searchPastDate").value;
   searchEvent(pastDate);
@@ -14,15 +14,14 @@ searchButton.addEventListener("click", function () {
 var contentT = document.getElementById("content");
 
 
-async function searchEvent() {
-	var pastDate;
+async function searchEvent(pastDate) {
 	console.log("pastDate:");
 	console.log(pastDate);
   let response;
   try {
     const request = {
       calendarId: "primary",
-      timeMin: pastDate,
+      timeMin: (new Date(Date.parse(pastDate))).toISOString(),
       showDeleted: false,
       singleEvents: true,
       maxResults: 10,
@@ -50,45 +49,4 @@ async function searchEvent() {
   );
   contentT.innerHTML = output;
 }
-
-// Refer to the JavaScript quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/js
-// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-// stored credentials.
-
-// const event = {
-//     'summary': 'Google I/O 2015',
-//     'location': '800 Howard St., San Francisco, CA 94103',
-//     'description': 'A chance to hear more about Google\'s developer products.',
-//     'start': {
-//       'dateTime': '2015-05-28T09:00:00-07:00',
-//       'timeZone': 'America/Los_Angeles'
-//     },
-//     'end': {
-//       'dateTime': '2015-05-28T17:00:00-07:00',
-//       'timeZone': 'America/Los_Angeles'
-//     },
-//     'recurrence': [
-//       'RRULE:FREQ=DAILY;COUNT=2'
-//     ],
-//     'attendees': [
-//       {'email': 'lpage@example.com'},
-//       {'email': 'sbrin@example.com'}
-//     ],
-//     'reminders': {
-//       'useDefault': false,
-//       'overrides': [
-//         {'method': 'email', 'minutes': 24 * 60},
-//         {'method': 'popup', 'minutes': 10}
-//       ]
-//     }
-//   };
-
-//   const request = gapi.client.calendar.events.insert({
-//     'calendarId': 'primary',
-//     'resource': event
-//   });
-
-//   request.execute(function(event) {
-//     appendPre('Event created: ' + event.htmlLink);
-//   });
+/////////////////////////////////S////////////////////////////////////////
