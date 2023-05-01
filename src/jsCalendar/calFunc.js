37bbdb613ef4fbf6ef14cd54dc28e57b37f45039
Manuel Hartmann <manuel.hartmann@stud.htw-dresden.de>
2023-05-01 12:34:32 +0200

    create Event statisch

--- src/jsCalendar/calFunc.js
+++ src/jsCalendar/calFunc.js
@@ -53,3 +53,50 @@ async function searchEvent(pastDate, searchValue) {
 }
 /////////////////////////////////S////////////////////////////////////////
 
+
+/////////////////////////////////create Event////////////////////////////////////////
+//Listener for createEvent button
+var createEventButton = document.getElementById("createEvent");
+createEventButton.addEventListener("click", function () {
+  const event = {
+    'summary': 'Meeting',
+    'location': 'HTW-Dresden',
+    'description': 'Meeting',
+    'start': {
+      'dateTime': '2023-05-03T15:00:00+02:00',
+      'timeZone': 'Europe/Berlin'
+    },
+    'end': {
+      'dateTime': '2023-05-03T16:00:00+02:00',
+      'timeZone': 'Europe/Berlin'
+    }
+    // 'recurrence': [
+    //   'RRULE:FREQ=DAILY;COUNT=2'
+    // ],
+    // 'attendees': [
+    //   {'email': 'lpage@example.com'},
+    //   {'email': 'sbrin@example.com'}
+    // ],
+    // 'reminders': {
+    //   'useDefault': false,
+    //   'overrides': [
+    //     {'method': 'email', 'minutes': 24 * 60},
+    //     {'method': 'popup', 'minutes': 10}
+    //   ]
+    // }
+  };
+  
+ 
+  const request = gapi.client.calendar.events.insert({
+    'calendarId': 'primary',
+    'resource': event
+  });
+
+  request.execute(function(event) {
+    // appendPre('Event created: ' + event.htmlLink);
+  });
+  
+});
+
+
+/////////////////////////////////E////////////////////////////////////////
