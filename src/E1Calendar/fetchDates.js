c3614137037ca3f303b316d0cf4d15222b36eea3
Jamal Alkharrat <s82035@htw-dresden.de>
2023-05-13 16:15:20 +0200

    Calender render, fetch dates

---
+++ src/E1Calendar/fetchDates.js
@@ -0,0 +1,66 @@
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+var eventsArray;
+var today = new Date();
+
+var fetchButton = document.getElementById("fetchButton");
+var contentT = document.getElementById("content");
+fetchButton.addEventListener("click", function () {
+  fetchDates(today);
+});
+
+async function fetchDates(today){
+  // extract end and start date from today
+  var startDate = new Date(today.getFullYear(), today.getMonth(), 1); // today -1 month
+  startDate.setDate(startDate.getDate() - 14); // Subtract 2 weeks
+  var startDateFormatted = formatDate(startDate);
+
+  var endDate = new Date(today.getFullYear(), today.getMonth() + 2, 1); // today +2 month
+  endDate.setDate(endDate.getDate() + 14); // Add 2 weeks
+  var endDateFormatted = formatDate(endDate);
+  // debug
+  console.log("fetchDates: " + startDateFormatted + " - " + endDateFormatted);
+  let response;
+  try {
+    // Request Oject
+    const request = {
+      calendarId: "primary",
+      timeMin: new Date(Date.parse(startDateFormatted)).toISOString(),
+      timeMax: new Date(Date.parse(endDateFormatted)).toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 999,
+      orderBy: "startTime",
+    };
+    // Debug
+    //console.log(request);
+    // Send request to Google Calendar API and save as response
+    response = await gapi.client.calendar.events.list(request);
+  } catch (err) {
+    // Error handling
+    console.log(err.message);
+    return;
+  }
+  // Save events from response
+  const events = response.result.items;
+  // Flatten events into an array of objects
+  eventsArray = events.map(event => ({
+    title: event.summary,
+    start: event.start.dateTime || event.start.date,
+    end: event.end.dateTime || event.end.date
+  }));
+  // debug
+  console.log(eventsArray);
+  return eventsArray;
+}
+export { eventsArray , fetchDates };
---
+++ src/E1Calendar/login.js
@@ -0,0 +1,108 @@
+
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '966425262226-nflro5si4ftpk7c3c4hq57ngrcr70hsn.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCSEoZIXZoR9SOyNUnAIpMnWStJQRN41Cc';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signed_in').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
+}
+
+/*
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+        document.getElementById('authorize_button').style.visibility = 'visible';
+    }
+}
+
+//Redirects to the main page after Login.
+// function NewSite() {
+//     window.location.replace("index2.html")
+// }
+
+//Sign in the user upon button click.
+function handleAuthClick() {
+    tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+        throw (resp);
+        }
+        // NewSite();
+        document.getElementById('signed_in').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        // await searchEvent();
+    };
+
+    if (gapi.client.getToken() === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({prompt: ''});}
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+        google.accounts.oauth2.revoke(token.access_token);
+        gapi.client.setToken('');
+
+    
+        document.getElementById('content').innerText = '';
+        document.getElementById('authorize_button').innerText = 'Authorize';
+        document.getElementById('signed_in').style.visibility = 'hidden';
+    }
+    // window.location.replace("index.html");
+}
