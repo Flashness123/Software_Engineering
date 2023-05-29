77a421ac74349a99d0d31b6d54786016790ec080
Manuel Hartmann <manuel.hartmann@stud.htw-dresden.de>
2023-05-29 12:38:24 +0200

    json

--- src/E1Calendar/updateEvent.js
+++ src/E1Calendar/updateEvent.js
@@ -73,3 +73,24 @@ async function updateEvent(){
     });
    
 }
+
+function fjson(){
+  console.log("json function");
+  loadJSON('test.json', function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+    console.log(data.result.result_text[0]);
+  });
+}
+function loadJSON(file,callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType('application/json');
+  xobj.open('GET', file, true); 
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+    // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+     }
+  };
+  xobj.send(null);  
+}
